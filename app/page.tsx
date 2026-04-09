fc9efa0fbca2a51799a6ecd2864c304e9a87d042
tayyabjamshaid <tayyab.devblends@gmail.com>
2026-04-10 02:27:05 +0500

    feat :  animation section updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
+import { CodeDemo } from "@/components/demo-components-animate-code";
 import { StarsBackgroundDemo } from "@/components/demo-components-backgrounds-stars";
-import { GrayTitleComponent ,GoldTitleComponent} from "@/components/reusables";
+import { GrayTitleComponent, GoldTitleComponent } from "@/components/reusables";
 import { Badge } from "@/components/ui/badge";
-
+import { Button } from "@/components/ui/button";
+import { AVATARS, LOGOS } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black overflow-x-hidden">
-   <section className="min-h-screen p-10 pt-28 sm:pt-32 relative grid grid-cols-1 lg:grid-cols-5 px-4 sm:px-8 pb-20 overflow-hidden"> 
-    <StarsBackgroundDemo/>
-       <div className="col-span-full lg:col-span-3 flex flex-col items-center justify-center text-center lg:-rotate-2">
+      <section className="min-h-screen p-10 pt-28 sm:pt-32 relative grid grid-cols-1 lg:grid-cols-5 px-4 sm:px-8 pb-20 overflow-hidden">
+        <StarsBackgroundDemo />
+        <div className="col-span-full lg:col-span-3 flex flex-col items-center justify-center text-center lg:-rotate-2">
           <Badge variant="gold">Powered by AI — Now in Beta</Badge>
 
           <h1 className="font-serif relative text-5xl sm:text-6xl lg:text-7xl tracking-tighter max-w-4xl">
@@ -22,8 +26,82 @@ export default function Home() {
             Get AI-powered feedback, role-specific questions, and the confidence
             to land your dream job.
           </p>
+
+          <div className="relative flex justify-center gap-2 sm:gap-4 mt-10 sm:w-auto">
+            <Link href="/onboarding">
+              <Button variant="gold" size="hero">
+                Get started
+              </Button>
+            </Link>
+
+            <Link href="/explore">
+              <Button variant="outline" size="hero">
+                Browse Interviewers →
+              </Button>
+            </Link>
           </div>
-   </section>
-  </div>
+
+          <div className="relative flex items-center justify-center gap-3 sm:gap-4 mt-8 sm:mt-16">
+            <div className="flex">
+              {AVATARS.map((av, i) => (
+                <div
+                  key={i}
+                  className={`w-8 h-8 rounded-full border-2 border-[#0a0a0b] overflow-hidden ${
+                    i > 0 ? "-ml-2" : ""
+                  }`}
+                >
+                  <Image
+                    src={av.src}
+                    alt="user avatar"
+                    width={32}
+                    height={32}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+              ))}
+            </div>
+
+            <p className="text-sm text-stone-500 text-center sm:text-left">
+              <strong className="text-stone-400 font-medium">
+                2,400+ engineers
+              </strong>{" "}
+              cracked FAANG interviews via Prept
+            </p>
+          </div>
+        </div>
+        {/* RIGHT */}
+        <div className="col-span-full lg:col-span-2 flex items-center justify-center lg:justify-start mt-12 lg:mt-0 lg:rotate-3">
+          {/* <Image
+            src="/hero.png"
+            alt="Hero illustration"
+            width={600}
+            height={400}
+            className="w-full max-w-md sm:max-w-lg lg:max-w-none h-auto"
+            priority
+          /> */}
+
+          <CodeDemo duration={30000} writing />
+        </div>
+      </section>
+      {/* LOGOS */}
+      <section className="relative z-10 border-y border-white/10 py-14">
+        <p className="text-center text-xs font-medium text-stone-600 tracking-widest uppercase mb-8">
+          Interviewees landed roles at
+        </p>
+
+        <div className="flex flex-wrap items-center justify-center gap-24 px-6">
+          {LOGOS.map((l) => (
+            <Image
+              key={l.alt}
+              src={l.src}
+              alt={l.alt}
+              width={50}
+              height={50}
+              className="h-6 w-auto opacity-60 grayscale"
+            />
+          ))}
+        </div>
+      </section>
+    </div>
   );
-} 
+}
