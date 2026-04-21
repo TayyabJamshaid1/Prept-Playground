7541a1e9cdaf7ecfc1b39e7c5513d3d4a591db3f
tayyabjamshaid <tayyab.devblends@gmail.com>
2026-04-21 16:52:33 +0500

    feat : migrations added

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,12 +3,24 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+
+// export default defineConfig({
+//   schema: "prisma/schema.prisma",
+//   migrations: {
+//     path: "prisma/migrations",
+//   },
+//   datasource: {
+//     url: process.env["DIRECT_URL"],
+//   },
+// });
+
+import 'dotenv/config'
+
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
   datasource: {
-    url: process.env["DIRECT_URL"],
+    url: process.env.DATABASE_URL!,
   },
-});
+  migrations: {
+    path: "./prisma/migrations",
+  },
+})
